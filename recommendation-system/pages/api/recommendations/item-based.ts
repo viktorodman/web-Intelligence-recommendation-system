d2758465f4acb3b359233ab6b5d9324298959719
Viktor Ödman <vo222dq@student.lnu.se>
2021-11-19 16:50:18 +0100

    Working on itembased

---
+++ recommendation-system/pages/api/recommendations/item-based.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { MatchingMovie } from '../../../types/matching-movie'
+import { findItemBasedRecommendationsEuclidean, findRecommendedMoviesEuclidean } from '../../../utils/euclidean-calculations'
+import { findRecommendedMoviesPearson } from '../../../utils/pearson-calculations'
+
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<MatchingMovie[]>) {
+    const { userId,  simMethod, numOfResults } = req.query
+
+    console.log(userId,simMethod,numOfResults)
+
+    if (!userId || !simMethod || !numOfResults){
+        res.status(400).json([])  
+    }
+    
+    if (Number(simMethod) === 1) {
+        const data = await findItemBasedRecommendationsEuclidean(Number(userId))
+        res.status(200).json(data)
+    } else if(Number(simMethod) === 2) {
+        const data = await findRecommendedMoviesPearson(Number(userId))
+        res.status(200).json(data.slice(0, Number(numOfResults)))
+    } else {
+        res.status(400).json([])
+    }
+}
--- recommendation-system/utils/euclidean-calculations.ts
+++ recommendation-system/utils/euclidean-calculations.ts
@@ -6,6 +6,17 @@ import { UserRatings } from "../types/user-ratings"
 import { readMoviesFromFile } from "./file-reader"
 import { getUserRatings, getWeightedScores } from "./helper"
 
+type UserMovieScore = {
+    username: string
+    score: number
+}
+
+type MovieWithUsers = {
+    id: number
+    title: string
+    users: UserMovieScore[]
+}
+
 export const findTopMatchingUsersEuclidean = async (userId: number): Promise<MatchingUser[]> => {
     const userRatings: UserRatings[] = await getUserRatings()
     const passedUserRating = userRatings.find(ur => ur.userId === userId)
@@ -28,8 +39,6 @@ export const findRecommendedMoviesEuclidean = async (userId: number): Promise<Ma
         return !(passedUserRating?.ratings.find(f => f.movieId === x.id))
     })
 
-    
-    
     if (!passedUserRating) {
         return []
     }
@@ -40,8 +49,74 @@ export const findRecommendedMoviesEuclidean = async (userId: number): Promise<Ma
     return calcWeightedScores.sort((a,b) => b.score - a.score)
 }
 
-export const findItemBasedRecommendationsEuclidean = async () => {
-    
+export const findItemBasedRecommendationsEuclidean = async (userId: number): Promise<any[]> => {
+    const movies = await readMoviesFromFile()
+    const userRatings: UserRatings[] = await getUserRatings()
+    const passedUserRating = userRatings.find(ur => ur.userId === userId)
+
+    const temp: MovieWithUsers[] = movies.map(movie => ({ id: movie.id, title: movie.title, users: [] }))
+
+    userRatings.forEach(user => {
+        user.ratings.forEach(rating => {
+            const movie = temp.find(f => f.id === rating.movieId)
+
+            movie?.users.push({score: rating.score, username: user.username})
+        })
+    })
+
+    const filterOutAlreadySeenMovies = temp.filter(x => {
+        return !(passedUserRating?.ratings.find(f => f.movieId === x.id))
+    })
+
+    const temp2 = getEuclideanScoresItemBased(temp, filterOutAlreadySeenMovies)
+
+
+    /* return filterOutAlreadySeenMovies.sort((a, b) => b.users.length - a.users.length) */
+    return temp2
+}
+
+const getEuclideanScoresItemBased = (userRatings: MovieWithUsers[], passedUserRating: MovieWithUsers[]): any[] => {
+    const scores:any[][] = []
+
+    userRatings.forEach(ur => {
+        const temp:any[] = []
+        passedUserRating.forEach(pur => {
+            if (ur.id !== pur.id) {
+                temp.push({
+                    idyeah: pur.id,
+                    id: ur.id,
+                    name: ur.title,
+                    nameyeah: pur.title,
+                    score: euclideanItemBased(ur, pur)
+                })
+            }
+        })
+        scores.push(temp)
+    })
+
+
+    return scores
+}
+
+const euclideanItemBased = (movieA: MovieWithUsers, movieB: MovieWithUsers): number => {
+    let sim = 0
+    let n = 0
+
+    movieA.users.forEach(movieAUser => {
+        movieB.users.forEach(movieBUser => {
+            
+            if (movieAUser.username === movieBUser.username) {
+                sim += ((movieAUser.score - movieBUser.score)**2)
+                n += 1
+            }
+        })
+    })
+
+    if(n === 0) {
+        return 0
+    }
+
+    return (1 / (1+sim))
 }
 
 const euclidean = (userA: UserRatings, userB: UserRatings): number => {
@@ -64,6 +139,8 @@ const euclidean = (userA: UserRatings, userB: UserRatings): number => {
 
     return (1 / (1+sim))
 }
+
+
 
 const getEuclideanScores = (userRatings: UserRatings[], passedUserRating: UserRatings): SimilarityRating[] => {
     const scores: SimilarityRating[] = []
